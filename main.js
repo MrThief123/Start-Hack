d571d7a9e2141e95e2b10d6a874fa32be359da9a
MrThief123 <ishan.d.mail@gmail.com>
2024-06-30 16:08:13 +1000

    stuff

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Wow Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
