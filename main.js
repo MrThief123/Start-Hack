019cb13750b9cd9b72838726de916d52d2bd4ece
MrThief123 <ishan.d.mail@gmail.com>
2024-06-30 16:43:11 +1000

    .

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.querySelector('#app').innerHTML = `
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
+    
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
