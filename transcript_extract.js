3c24c81c69dd0987aed2469b6c85aa9f4501e937
MrThief123 <ishan.d.mail@gmail.com>
2024-07-01 18:32:01 +1000

    commeents

--- transcript_extract.js
+++ transcript_extract.js
@@ -3,15 +3,16 @@ export function setupFetchAndDisplaySubtitles(goButton, urlInput) {
     const videoUrl = urlInput.value.trim(); // Assuming urlInput is an input element
 
     try {
+      // send the url to proxy server to extract transcipt
       const response = await fetch(`http://localhost:3003/fetchTranscript?url=${encodeURIComponent(videoUrl)}`);
       if (!response.ok) {
         throw new Error('Failed to fetch transcript');
       }
 
-      const transcriptText = await response.text(); // Get the plain text response
+      const transcriptText = await response.text(); 
       
-      console.log('Transcript:', transcriptText); // Logging transcript to console
-      alert(transcriptText); // Displaying transcript as an alert
+      console.log('Transcript:', transcriptText); 
+      alert(transcriptText); // print the transcipt
     } catch (error) {
       console.error('Error fetching transcript:', error);
       alert('Error fetching transcript');
