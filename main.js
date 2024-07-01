e7a308a6a35811ef13c1ec3303cc233236b09f96
MrThief123 <ishan.d.mail@gmail.com>
2024-07-01 21:42:52 +1000

    cleanup

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
 // main.js
 import './style.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
 import { setupFetchAndDisplaySubtitles } from './transcript_extract.js'; // Corrected import path
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>This is H1 Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+  
+    <h1>Hello!</h1>
+
     
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
     
     <div>
       <input type="text" id="urlInput" placeholder="Enter a URL" />
@@ -29,7 +15,6 @@ document.querySelector('#app').innerHTML = `
   </div>
 `;
 
-setupCounter(document.querySelector('#counter'));
 
 // Now call setupYoutubeCaptions after setting up the DOM
 setupFetchAndDisplaySubtitles(document.querySelector('#goButton'), document.querySelector('#urlInput'));
